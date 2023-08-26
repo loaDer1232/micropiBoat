df5df48d39a967ef0f2db4f00525b265f6e4bd86
baksteennz <christoeras@gmail.com>
2023-08-26 16:42:27 +1200

    added a bunch of stuff

--- main.py
+++ main.py
@@ -1,39 +1,78 @@
 from servo import Servo
 from machine import Pin
+from customError import *
 import utime
 
 mainServo = Servo(20)
 jibServo = Servo(21)
 led = Pin("LED", Pin.OUT)
 
-def servo_Map(x, in_min, in_max, out_min, out_max):
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
- 
-def servo_Angle(angle):
-    if angle < 0:
-        angle = 0
-    if angle > 180:
-        angle = 180
-    mainServo.goto(round(servo_Map(angle,0,180,0,1024))) # Convert range value to angle value
+def servo_Map(x: int, in_min: int, in_max: int, out_min: int, out_max: int) -> int:
+    return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
+
+def servo_Angle_Main(angle: int):
+    if (angle < 0) or (angle > 180):
+        raise AngleNotInRangeError("main")
+    mainServo.goto(round(servo_Map(angle,0,180,0,1024)))
+
+def servo_Angle_Jib(angle: int):
+    if (angle < 0) or (angle > 180):
+        raise AngleNotInRangeError("jib")
     jibServo.goto(round(servo_Map(angle,0,180,0,1024)))
 
-def POST(x):   
-    while x:
-        print("Turn left ...")
-        for i in range(0,180,10):
-            servo_Angle(i)
-            utime.sleep(0.05)
-        print("Turn right ...")
-        for i in range(180,0,-10):
-            servo_Angle(i)
-            utime.sleep(0.05)
-        x = False
-        main()
+def POST():   #POST tests if the servos are broken
+    print("Turn left ...")
+    for i in range(0,180,10):
+        servo_Angle_Main(i)
+        servo_Angle_Jib(i)
+        utime.sleep(0.05)
+    print("Turn right ...")
+    for i in range(180,0,-10):
+        servo_Angle_Main(i)
+        servo_Angle_Jib(i)
+        utime.sleep(0.05)
+    main()
 
-def main():
-    if angel():
-        
-if __name__ == '__main__':
-    x = True
-    POST(x)
+def points_Sail_Main(windAngle: float) -> int:
+    downWind: int = 0
+    broadReach: int = 50
+    beamReach: int = 100
+    closeHaul: int = 180
+    if(windAngle > 45) and (windAngle < 90):
+        return closeHaul 
+    if(windAngle > 90) and (windAngle < 135):
+        return beamReach
+    if(windAngle > 135) and (windAngle < 180):
+        return broadReach
+    if windAngle == 180:
+        return downWind
+    raise InvaledWindAngleError
+
+def points_Sail_Jib(windAngle: float) -> int:
+    downWind: int = 0
+    broadReach: int = 25
+    beamReach: int = 120
+    closeHaul: int = 180
+    if(windAngle > 45) and (windAngle < 90):
+        return closeHaul 
+    if(windAngle > 90) and (windAngle < 135):
+        return beamReach
+    if(windAngle > 135) and (windAngle < 180):
+        return broadReach
+    if windAngle == 180:
+        return downWind
+    raise InvaledWindAngleError
+
+
+def main(windAngle: float):
+    led.high
+    while True:
+        try:
+            servo_Angle_Main(points_Sail_Main(windAngle))
+            servo_Angle_Jib(points_Sail_Jib(windAngle))
+        except:
+            print("start failed")
+            POST()
+            
+POST()
 
